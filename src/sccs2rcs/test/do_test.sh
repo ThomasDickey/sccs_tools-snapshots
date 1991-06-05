387d347e97c69a854738dd3cb419564e1440e141
Thomas E. Dickey <dickey@invisible-island.net>
1991-06-05 17:43:35 -0400

    snapshot of project "sccs_tools", label t19910605

--- src/sccs2rcs/test/do_test.sh
+++ src/sccs2rcs/test/do_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: do_test.sh,v 2.1 1989/10/10 15:27:40 dickey Exp $
+# $Id: do_test.sh,v 3.0 1991/06/05 17:43:35 ste_cm Rel $
 # Run a specified regression test on the sccs/RCS conversion utility.
 # Arguments:
 #	$1 = option(s) for sccs2rcs
@@ -7,15 +7,25 @@
 #	$3+ = version numbers which are defined in the file.
 #
 # $Log: do_test.sh,v $
-# Revision 2.1  1989/10/10 15:27:40  dickey
-# use current directory as RCS-directory for testing
+# Revision 3.0  1991/06/05 17:43:35  ste_cm
+# BASELINE Tue Jun 18 08:04:39 1991 -- apollo sr10.3
 #
+# Revision 2.2  91/06/05  17:43:35  dickey
+# push-aside pre-existing RCS-directory so this script will work ok.
+# 
+# Revision 2.1  89/10/10  15:27:40  dickey
+# use current directory as RCS-directory for testing
+# 
 # Revision 1.2  89/03/23  08:20:17  dickey
 # quote "$O" variable so we can pass in quoted string (testfile3 case)
 # 
 # Revision 1.1  89/03/22  09:37:06  dickey
 # Initial revision
 # 
+#
+if test -d RCS
+then	mv RCS RCS-
+fi
 #
 D=.
 X=../bin/sccs2rcs
@@ -46,3 +56,6 @@ do
 	rm -f $F.RCS $F.SCCS
 done
 rm -f $D/$F,v
+if test -d RCS-
+then	mv RCS- RCS
+fi
